df431e00645339d6012bfed165a3f6d2ac49e4d4
ANMOL MISHRA <mishraanmol691@gmail.com>
2024-10-27 17:58:13 +0530

    Add ResetPassword controller

--- server/routes/User.js
+++ server/routes/User.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const {login,signup,sendOTP,changePassword,} = require("../controllers/Auth")
-// const {resetPasswordToken,resetPassword, } = require("../controllers/ResetPassword")
+const {resetPasswordToken,resetPassword, } = require("../controllers/ResetPassword")
 
 const {auth,isStudent, isInstructor,isAdmin }= require("../middlewares/auth");
 
@@ -10,7 +10,7 @@ router.post("/login", login);
 router.post("/signup", signup);
 router.post("/sendOTP", sendOTP);
 
-// router.get("/test",auth);
+
 router.get("/test",auth,(req,res)=>{res.json({success:true,message:"ITs just authentic test route ", });});
 router.get("/student",auth,isStudent,(req,res)=>{res.json({success:true,message:"Welcome to student route", });});
 router.get("/admin",auth,isAdmin,(req,res)=>{res.json({success:true,message:"Welcome to student route", });});
@@ -19,8 +19,8 @@ router.get("/instructor",auth,isInstructor,(req,res)=>{res.json({success:true,me
 
 router.post("/changepassword",auth,changePassword);
 
-// router.post("/reset-password-token", resetPasswordToken);
-// router.post("/reset-password", resetPassword);
+router.post("/reset-password-token", resetPasswordToken);
+router.post("/reset-password", resetPassword);
 
 
 module.exports = router;
